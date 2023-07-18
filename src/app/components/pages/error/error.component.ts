987c2ea00e8e9cacf59ee1f334c99ea8c5846be7
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-07-17 21:17:46 -0300

    css page error

--- src/app/components/pages/error/error.component.ts
+++ src/app/components/pages/error/error.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./error.component.css']
 })
 export class ErrorComponent {
+
+  constructor(
+    private router: Router
+  ) {}
+
+  homeBack():void {
+    this.router.navigate(['/'])
+  }
 
 }
