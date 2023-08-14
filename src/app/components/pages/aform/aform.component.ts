3bbfcc06d5d86bb780bb7816f6698162e275e77b
MatheusT <matheusgarciajbs@gmail.com>
2023-08-14 15:13:17 -0300

    form html

--- src/app/components/pages/aform/aform.component.ts
+++ src/app/components/pages/aform/aform.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { IesSelecionadaService } from 'src/app/service/ies-selecionada.service';
+import { CursoSelecionadoService } from 'src/app/service/curso-selecionado.service';
 
 @Component({
   selector: 'app-aform',
   templateUrl: './aform.component.html',
   styleUrls: ['./aform.component.css'],
 
-  
+
 })
 export class AformComponent {
+
+  ies!:any
+  curso!:any
+
+  nome!:any
+
+  constructor(
+    private iesSelecionada:IesSelecionadaService,
+    private cursoSelecionado:CursoSelecionadoService
+  ) {}
+
+  ngOnInit() {
+    this.ies = this.iesSelecionada.getIesSelecionada()
+    this.curso = this.cursoSelecionado.getCursoSelecionado()
+    this.nome = this.ies.iesEnviar.nome
+  }
+
+  teste() {
+    console.log(
+    this.ies
+
+    )
+  }
 
 }
 
